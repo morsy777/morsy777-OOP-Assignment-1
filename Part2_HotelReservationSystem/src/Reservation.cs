7582a57de962ac39473875d98ec5a552dae9e853
Mohamed Morsi <777medooo@gmail.com>
2026-09-20 06:18:47 +0300

    feat: Hotel Reservation System

---
+++ Part2_HotelReservationSystem/src/Reservation.cs
@@ -0,0 +1,77 @@
+﻿namespace Part2_HotelReservationSystem;
+
+public class Reservation
+{
+    public int Id { get; }
+    public DateTimeOffset CheckInDate { get; }
+    public DateTimeOffset CheckOutDate { get; }
+    
+    public ReservationStatus  Status { get; private set; }
+    public Room Room { get; private set; }
+    
+    public Reservation(int id, DateTimeOffset checkInDate, DateTimeOffset checkOutDate, Room room)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(id);
+        
+        ArgumentNullException.ThrowIfNull(room);
+        
+        if(room.HasReservation(checkInDate, checkOutDate))
+            throw new ArgumentException("Room already has a reservation.");
+        
+        if(room.IsUnderMaintenance)
+            throw new ArgumentException("Room is under maintenance");
+        
+        if (!IsValidDate(checkInDate, checkOutDate))
+            throw new ArgumentException("Check out date must be after check in date");
+        
+        Id = id;
+        CheckInDate = checkInDate;
+        CheckOutDate = checkOutDate;
+        Room = room;
+    }
+
+    public decimal GetTotalCost()
+    {
+        int nights  = (CheckOutDate - CheckInDate).Days;
+        return Room.NightlyRate * nights;
+    }
+    
+    // Pending -> Confirm
+    public void Confirm()
+    {
+        if (Status != ReservationStatus.Pending)
+            throw new InvalidOperationException("Only pending reservations can be confirmed.");
+
+        Status = ReservationStatus.Confirmed;
+    }
+    
+    // Confirm -> Check in
+    public void CheckIn()
+    {
+        if (Status != ReservationStatus.Confirmed)
+            throw new InvalidOperationException("Only confirmed reservations can be checked in.");
+        
+        Status = ReservationStatus.CheckedIn;
+    }
+    
+    // Check in -> Check out
+    public void CheckOut()
+    {
+        if (Status != ReservationStatus.CheckedIn)
+            throw new InvalidOperationException("Only checked in reservations can be checked out.");
+        
+        Status = ReservationStatus.CheckedOut;
+    }
+    
+    // only pending, confirmed -> canceled
+    public void Cancel()
+    {
+        if (Status != ReservationStatus.Pending && Status != ReservationStatus.Confirmed)
+            throw new InvalidOperationException("Only pending or confirmed reservations can be cancelled.");
+        
+        Status = ReservationStatus.Cancelled;
+    }
+
+    private bool IsValidDate(DateTimeOffset checkInDate, DateTimeOffset checkOutDate)
+        => checkInDate < checkOutDate;
+}
---
+++ Part2_HotelReservationSystem/src/Room.cs
@@ -0,0 +1,55 @@
+﻿namespace Part2_HotelReservationSystem;
+
+public class Room
+{
+    public int Number { get; }
+    public RoomType RoomType { get; }
+    public decimal NightlyRate { get; private set; }
+    public bool IsUnderMaintenance { get; private set; }
+    
+    private readonly List<Reservation> _reservations = [];
+
+    public IReadOnlyList<Reservation> Reservations => _reservations;
+
+    public Room(int number, decimal nightlyRate, RoomType roomType)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(number); 
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(nightlyRate); 
+        
+        Number = number;
+        NightlyRate = nightlyRate;
+        RoomType = roomType;
+    }
+
+    public bool HasReservation(DateTimeOffset checkInDate, DateTimeOffset checkOutDate)
+    {
+        bool hasReservation = false;
+        foreach (var reservation in Reservations)
+        {
+            if (reservation.Status != ReservationStatus.Cancelled
+                && reservation.Status != ReservationStatus.CheckedOut
+                && reservation.CheckOutDate > checkInDate
+                && reservation.CheckInDate < checkOutDate)
+            {
+                hasReservation = true;
+                break;
+            }
+        }
+        
+        return hasReservation;
+    }
+    
+    public void ChangeNightlyRate(decimal newRate)
+    {
+        if (newRate < 0)
+            throw new ArgumentOutOfRangeException(nameof(newRate));
+
+        NightlyRate = newRate;
+    }
+    
+    public void StartMaintenance() 
+        => IsUnderMaintenance = true;
+    
+    public void EndMaintenance()
+        => IsUnderMaintenance = false;
+}
